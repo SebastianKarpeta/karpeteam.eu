8865b6722e9f12ea8bbbd65687aa775a9ba92f14
SebastianKarpeta <sebastian@karpeta.org>
2026-01-29 12:46:52 +0100

    Update admin with models Category and Posts.

--- kpt/admin.py
+++ kpt/admin.py
@@ -1,5 +1,34 @@
 from django.contrib import admin
-from .models import Post
+from django.utils.html import format_html
+from .models import Post, Category
 
-admin.site.register(Post)
 
+# ------------------ POST ADMIN ------------------
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('short_title', 'category', 'author', 'is_active', 'create_date')
+    list_editable = ('is_active',)
+    list_filter = ('is_active',)
+    search_fields = ('title', 'text')
+
+    # Skrócony tytuł z tooltipem
+    def short_title(self, obj):
+        if len(obj.title) > 100:
+            return format_html('<span title="{}">{}...</span>', obj.title, obj.title[:100])
+        return obj.title
+    short_title.short_description = 'Tytuł'
+
+
+# ------------------ CATEGORY ADMIN ------------------
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('short_title', 'is_active', 'create_date')
+    list_editable = ('is_active',)
+    list_filter = ('is_active',)
+
+    # Skrócony tytuł z tooltipem
+    def short_title(self, obj):
+        if len(obj.title) > 100:
+            return format_html('<span title="{}">{}...</span>', obj.title, obj.title[:100])
+        return obj.title
+    short_title.short_description = 'Tytuł'
--- kpt/models.py
+++ kpt/models.py
@@ -1,18 +1,45 @@
 from django.db import models
 from django.utils import timezone
+from django_ckeditor_5.fields import CKEditor5Field
+from django.utils.translation import gettext_lazy as _
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateField(auto_now_add=True)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=False, verbose_name=_('Aktywny?'))
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+
 
 class Post(models.Model):
-        author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-        title = models.CharField(max_length=200)
-        text = models.TextField()
-        create_date = models.DateTimeField(default=timezone.now)
-        publish_date = models.DateTimeField(blank=True, null=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        blank = True,
+        null = True
+    )
+    title = models.CharField(max_length=200)
+    text = CKEditor5Field(
+        config_name='default',
+        verbose_name='Treść artykułu'
+    )
+    create_date = models.DateField(auto_now_add=True)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=False, verbose_name=_('Aktywny?'))
 
-        def publish(self):
-            self.publish_date = timezone.now()
-            self.save()
+    def publish(self):
+        self.save()
 
-        def __str__(self):
-            return self.title
+    def __str__(self):
+        if self.category:
+            return f"{self.title} ({self.category.title})"
+        return self.title
 
-# Create your models here.
