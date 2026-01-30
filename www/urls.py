e83f35d4b4c056dd9a6b97df330cfbdd1c6b8e9a
SebastianKarpeta <sebastian@karpeta.org>
2026-01-30 09:37:38 +0100

    Adding redirect from main page to admin/

--- www/urls.py
+++ www/urls.py
@@ -19,8 +19,10 @@ from django.conf.urls.i18n import i18n_patterns
 from django.urls import path, include
 from django.contrib import admin
 from django.views.i18n import set_language
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),
     path('i18n/setlang/', set_language, name='set_language'),  # endpoint do zmiany języka
 ]
 
